f5e76646470ec4c8a01df85044eec89d0b3a4998
Gabriel Cirtea <gcirtea@yahoo.com>
2019-11-04 18:07:05 +0200

    Added menus to the app

--- app/src/main/java/com/example/bottomnavigationtests/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/bottomnavigationtests/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,11 @@
 package com.example.bottomnavigationtests.ui.dashboard
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.*
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.bottomnavigationtests.R
 import com.example.bottomnavigationtests.databinding.FragmentDashboardBinding
@@ -26,6 +24,35 @@ class DashboardFragment : Fragment() {
         val binding: FragmentDashboardBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_dashboard, container, false)
         binding.dashboardViewModel = dashboardViewModel
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.findItem(R.id.main_menu_categories).setVisible(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.main_menu_categories -> {
+            createPopupMenu(item)
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun createPopupMenu(item: MenuItem) {
+        PopupMenu(context, activity?.findViewById(R.id.main_menu_categories)).apply {
+            setOnMenuItemClickListener {
+                Toast.makeText(context, "You clicked on: ${it.title}", Toast.LENGTH_SHORT).show()
+                true
+            }
+            menu.add("Option 1")
+            menu.add("Option 2")
+            show()
+        }
     }
 }
